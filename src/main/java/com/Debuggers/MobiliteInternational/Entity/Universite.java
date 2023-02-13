5284a3eea20b9670563bcbddfd62e73f160fd0f5
Elloumi Abdellaziz <61619978+Ellouminati@users.noreply.github.com>
2023-02-13 22:56:58 +0100

    creation first Branch

--- src/main/java/com/Debuggers/MobiliteInternational/Entity/Universite.java
+++ src/main/java/com/Debuggers/MobiliteInternational/Entity/Universite.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Getter
 @Setter
@@ -15,7 +16,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="universite")
-public class Universite {
+public class Universite  implements Serializable {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
